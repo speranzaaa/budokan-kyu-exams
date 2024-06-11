3210fb480b05018e988b572875d3d840fe20a70f
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2024-06-11 12:01:08 +0200

    fix login

--- login.php
+++ login.php
@@ -10,7 +10,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $result = mysqli_query($conn, $query);
     $user = mysqli_fetch_assoc($result);
 
-    if ($user && password_verify($password, $user['password'])) {
+    if (isset($user) && password_verify($password, password_hash($user['password'], PASSWORD_BCRYPT))) {
         $_SESSION['user_id'] = $user['id'];
         header('Location: create_session.php');
         exit();
