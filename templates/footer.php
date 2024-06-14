bc8f9687f8454be84e6fbff30d9155c3bd19820b
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2024-06-14 12:35:19 +0200

    add axios support

--- templates/footer.php
+++ templates/footer.php
@@ -1,4 +1,5 @@
 <footer>
     <p>&copy; 2024 Exam Session Management</p>
     <script src="js/add_athlete.js"></script>
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 </footer>
