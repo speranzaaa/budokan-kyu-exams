eb462f45591cc226d16a2ec6f6bac03d0186cde1
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2024-06-14 19:07:00 +0200

    create db object and db_config

--- php/db.php
+++ php/db.php
@@ -9,4 +9,30 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-?>
+
+class DatabaseManager {
+
+    private $db;
+    public function __construct($servername, $username, $password, $dbname, $port) {
+        $this->db = new mysqli($servername, $username, $password, $dbname, $port);
+        if($this->db->connect_error) {
+            die("Connessione fallita al db");
+        }
+    }
+
+    public function __destruct()
+    {
+        $this->db->close();
+    }
+
+    public function searchAthleteFromId($id) {
+        $stmt = $this->db->prepare("SELECT * from atleti where id=?");
+        $stmt->bind_param("s", $id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_all(MYSQLI_ASSOC)[0];
+    }
+
+
+
+}
