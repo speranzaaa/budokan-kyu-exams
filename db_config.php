6393b23387b1139ee3a17c7a196ab726b4934997
Angela Speranza <angela.speranza@studio.unibo.it>
2024-06-16 18:36:32 +0200

    remove unused files

--- db_config.php
+++
@@ -1,4 +0,0 @@
-<?php
-session_start();
-require 'php/db.php';
-$dbh = new DatabaseManager("localhost", "root", "", "budokan", 3306);
--- password.php
+++
@@ -1,5 +0,0 @@
-<?php
-$password = 'password123';
-$hashed_password = password_hash($password, PASSWORD_BCRYPT);
-echo $hashed_password;
-?>
