99b82ea3fc68bbe7c68631d24ddffdc91ab8baf8
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2024-06-16 19:49:34 +0200

    Create api-insert-exam.php

---
+++ api/api-insert-exam.php
@@ -0,0 +1,35 @@
+<?php 
+
+include('../php/db.php');
+session_start();
+
+$session_id = $_POST['session_id'];
+$atleti_ids = $_POST['id'];
+
+if (isset($session_id)) {
+
+    foreach ($atleti_ids as $index => $id) {
+        $status = $_POST['exam_status'][$index];
+
+        if ($status !== 'Absent') {
+            $kihon_score = $_POST['kihon_score'][$index];
+            $kata_score = $_POST['kata_score'][$index];
+            $kumite_score = $_POST['kumite_score'][$index];
+
+            $average_score = ($kihon_score + $kata_score + $kumite_score) / 3;
+
+            echo $id. ' '. $session_id . ' '.$kihon_score. ' ' .$kata_score.' '.$kumite_score.' '.$average_score.' '.$status;
+            
+
+            $query1 = "INSERT INTO `esami`(`id_studente`, `session_id`, `kihon_score`, `kata_score`, `kumite_score`, `average`, `esito`) 
+            VALUES ('$id', '$session_id', '$kihon_score', '$kata_score', '$kumite_score', '$average_score', '$status')";
+            
+            mysqli_query($conn, $query1);
+
+            //$dbh->addToExamTable($id, $session_id, $kihon_score, $kata_score, $kumite_score, $average_score, $status);
+
+        }
+    }
+}
+
+//header('Location: ../create_session.php');
